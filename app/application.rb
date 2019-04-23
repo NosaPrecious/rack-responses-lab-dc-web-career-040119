3cae30aca73a6751304f7998ca31596286351f23
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-04-23 21:13:31 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application
+  
+  def call(env)
+    resp = Rack::Response
+    morning_t= Time.new(2019,04,23,9,30)
+    noon_t= Time.new(2019,04,23,14,30)
+    
+    if morning_t === Time.now
+      resp.write "Morning"
+      
+    else
+      resp.write "Afternoon"
+    end
+    
+    resp.finish
+  end
+end
